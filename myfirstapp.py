569f463592f07079f99dff2bdfb9e144e04edc46
T-Harng <78921082+T-Harng@users.noreply.github.com>
2022-09-03 10:44:56 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -17,7 +17,7 @@ if readme:
     st.write ("For more info, please contact:")
 
 #     st.write("<a href='https://www.linkedin.com/in/yong-poh-yu/'>Dr. Yong Poh Yu </a>", unsafe_allow_html=True)
-    st.write("<a href='https://mailto.thlo312@gmail.com'> TH </a>", unsafe_allow_html=True)
+#     st.write("<a href='https://mailto.thlo312@gmail.com'> TH </a>", unsafe_allow_html=True)
 
 option = st.sidebar.selectbox(
     'Select a mini project',
